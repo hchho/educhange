1c249b328b5858858837aca4fbcccbe584be33f9
Henry Ho <henrychho89@gmail.com>
2018-03-07 15:53:39 -0800

    Added signup form and page

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,8 @@ var config = {
     messagingSenderId: "654557831315"
 };
 
+var currUser;
+
 firebase.initializeApp(config);
 
 const app = express();
@@ -25,11 +27,28 @@ app.set('views', './views');
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
+    if(!currUser) {
+        console.log(currUser);
+    }
    res.render('index');
 });
 
 app.get('/signup', (req, res) => {
     res.render('signup');
-})
+});
+
+app.post('/signup', (req, res) => {
+    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
+    .then(function(user) {
+        currUser = {
+            email: req.body.email,
+            password: req.body.password
+        }
+        res.redirect('/');
+    })
+    .catch((err) => {
+        res.redirect('/signup');
+    });
+});
 
 exports.app = functions.https.onRequest(app);
