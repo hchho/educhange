4db669d1c37b96cce18478ae9930a6ab47205567
Henry Ho <henrychho89@gmail.com>
2018-03-08 09:42:59 -0800

    Testing commit

--- public/mainview.js
+++
@@ -1,6 +0,0 @@
-var database = firebase.database().ref().child('questions');
-
-database.on("child_added", function(data, prevChildKey) {
-   var question = data.val();
-   document.getElementById("questionText").innerHTML = question.question_text;
-});
