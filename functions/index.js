80d797873caf8e992dc139e91940343136ec51d3
Henry Ho <henrychho89@gmail.com>
2018-03-07 22:26:16 -0800

    Added signup and current user variable

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const engines = require('consolidate');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 var config = {
     apiKey: "AIzaSyB4WsRTYAg27pPz_Hdf-pLcaZPR-IxlHB0",
@@ -14,40 +16,82 @@ var config = {
     messagingSenderId: "654557831315"
 };
 
-var currUser;
-
 firebase.initializeApp(config);
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+var sess = {
+    secret: "damn-secretive",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}
+
+
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    sess.cookie.secure = true // serve secure cookies
+}
+
+var currUser;
+
+var sessionChecker = (req, res, next) => {
+    if(!currUser) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+var setUser = (user) => {
+    currUser = user;
+}
+
+app.use(session(sess));
+
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
-    if(!currUser) {
-        console.log(currUser);
-    }
-   res.render('index');
+app.get('/', sessionChecker, (req, res, next) => {
+//    console.log(currUser);
+    res.render('index', {"email" : currUser.email});
 });
 
-app.get('/signup', (req, res) => {
+app.get('/signup', (req, res, next) => {
     res.render('signup');
 });
 
-app.post('/signup', (req, res) => {
-    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
-    .then(function(user) {
-        currUser = {
-            email: req.body.email,
-            password: req.body.password
-        }
+app.post('/signup', (req, res, next) => {
+    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then((user) => {
+        setUser(user);
+        currUser.sendEmailVerification().then(function() {
+            console.log('Email sent');
+        }, function(error) {
+            console.log('Email not sent');
+        });
         res.redirect('/');
     })
-    .catch((err) => {
-        res.redirect('/signup');
+        .catch((err) => {
+        res.send(err);
+    });
+});
+
+app.get('/login', (req, res, next) => {
+    res.render('login'); 
+});
+
+app.post('/login', (req, res, next) => {
+    firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then((user) => {
+        setUser(user);
+        res.redirect('/');
+    })
+        .catch((err) => {
+        res.send(err);
     });
 });
 
